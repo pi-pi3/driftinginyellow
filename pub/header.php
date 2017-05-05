cc895e9fe3bc8f05fa63d34616b28a5a5fe555c8
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-05 17:41:34 +0200

    Add better localization

--- pub/header.php
+++ pub/header.php
@@ -18,14 +18,14 @@
 
     <div id="menu">
         <span class="left">
-            <a class="thisSite" href="/<?php echo $page['lang'] ?>/">home</a>
-            <a href="/<?php echo $page['lang'] ?>/games">Games</a>
-            <a href="/<?php echo $page['lang'] ?>/other">Other</a>
-            <a href="/<?php echo $page['lang'] ?>/cv">Curriculum Vitae</a>
+            <a class="thisSite" href="/<?php echo $page['lang'] ?>/"><?php echo $locale['home'] ?></a>
+            <a href="/<?php echo $page['lang'] ?>/games"><?php echo $locale['games'] ?></a>
+            <a href="/<?php echo $page['lang'] ?>/other"><?php echo $locale['other'] ?></a>
+            <a href="/<?php echo $page['lang'] ?>/cv"><?php echo $locale['cv'] ?></a>
         </span>
         <span class="right">
-            <a href="/<?php echo $page['lang'] ?>/about">About</a>
-            <a href="/<?php echo $page['lang'] ?>/contact">Contact</a>
+            <a href="/<?php echo $page['lang'] ?>/about"><?php echo $locale['about'] ?></a>
+            <a href="/<?php echo $page['lang'] ?>/contact"><?php echo $locale['contact'] ?></a>
 	    <a href="/<?php echo $page['nlang'] ?>/<?php if (!empty($page['name'])) echo $page['name'] . "/" ?>"><?php echo $page['nlang'] ?></a>
         </span>
     </div>
--- pub/preload.php
+++ pub/preload.php
@@ -1,9 +1,31 @@
 <?php
     $page = array();
-    $page['lang'] = 'en';
+
     $page['subtitle'] = 'Bringing yellow stars and games together since 1998...';
-    $page['nlang'] = 'de';
     $page['name'] = '';
+
+    $locale = array();
+    $page['lang'] = $lang;
+    if ($lang == 'en') {
+        $page['nlang'] = 'de';
+
+        $locale['home'] = 'home';
+        $locale['games'] = 'Games';
+        $locale['other'] = 'other';
+        $locale['cv'] = 'Curriculum Vitae';
+        $locale['about'] = 'About';
+        $locale['contact'] = 'Contact';
+    } else {
+        $page['nlang'] = 'en';
+
+        $locale['home'] = 'home';
+        $locale['games'] = 'Spiele';
+        $locale['other'] = 'Anderes';
+        $locale['cv'] = 'Curriculum Vitae';
+        $locale['about'] = 'Über mich';
+        $locale['contact'] = 'Kontakt';
+    }
+
     $template = array();
     $template['header'] = __DIR__ . '/header.php';
     $template['footer'] = __DIR__ . '/footer.php';
