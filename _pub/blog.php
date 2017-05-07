619551410966c6be4fc925bb2b6d7bb67d194d66
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-07 13:04:42 +0200

    Change article format

--- _pub/blog.php
+++ _pub/blog.php
@@ -2,12 +2,36 @@
     $articles = "articles";
     $handle = fopen($articles, "r");
     if ($handle) {
-        while (($filename = fgets($handle, 4096)) !== false) {
+        while (($line = fgets($handle, 4096)) !== false) {
+            $line = explode(' ', $line, 2);
+            $timestamp = trim($line[0]);
+            $date = date('l, jS F, Y H:i:s', intval($timestamp));
+
+            $filename = $line[1];
             $filename = trim($filename);
             $handle1 = fopen($filename, "r");
             $contents = fread($handle1, filesize($filename));
             fclose($handle1);
-            echo "<article>$contents</article";
+
+            $contents = explode('</header>', $contents, 2);
+            $header = $contents[0] . '</header>';
+            $contents = $contents[1]
+
+            $words = str_word_count($contents);
+            $minutes = intval(ceil($words / 250));
+
+            $m = 'minute';
+            if ($minutes > 1) {
+                $m = 'minutes';
+            }
+
+            // TODO: truncate contents to `n` words on main page
+            echo "<article>":
+            echo $header;
+            echo "<p style=\"font-size: 75%\">$date<br>";
+            echo "A $minutes $m read</p>";
+            echo $contents;
+            echo "</article>";
         }
         if (!feof($handle)) {
             echo "Error: unexpected fgets() fail\n";
