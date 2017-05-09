1ed575d4aaca9343bf3fcae2851325325e3df459
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-09 14:43:51 +0200

    Add footer

--- _backend/footer.php
+++ _backend/footer.php
@@ -1,3 +1,21 @@
     </div>
+
+    <div id="footer">
+        <p style="margin-bottom: 1px;">
+            Powered by a 5" hamster in a large running wheel hooked up to
+            an electromagnetic motor.
+        </p>
+        <p style="margin-bottom: 1px;">
+            This website and all images owned by me available on this website
+            are available under the
+            <a href="https://creativecommons.org/licenses/by-sa/4.0/">
+            Creative Commons Attribution-ShareAlike (CC BY-SA)</a> license
+            for free.
+        </p>
+        <p>
+            We do not provide any warranty for this website or services
+            it may offer.
+        </p>
+    </div>
 </body>
 </html>
