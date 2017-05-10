3c8365245228b1357cbc38edbe0167f1d2b2fe6c
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-10 22:23:01 +0200

    New new-article script

--- new-article.sh
+++ new-article.sh
@@ -1,21 +1,57 @@
 #!/usr/bin/env sh
-if [ -z $2 ]
+if [ -z $1 ]
 then
     lang=en
+    table=$1
+    id=$2
+    path=$3
+
+    if [ -z $4]
+    then
+        pinned='null'
+    else
+        pinned=$4
+    fi
+
+    if [ -z $5]
+    then
+        hide_meta='null'
+    else
+        hide_meta=$4
+    fi
+
+    if [ -z $5]
+    then
+        tags='null'
+    else
+        tags=$4
+    fi
 else
-    lang=$2
+    lang=$1
+    table=$2
+    id=$3
+    path=$4
+
+    if [ -z $5]
+    then
+        pinned='null'
+    else
+        pinned=$5
+    fi
+
+    if [ -z $6]
+    then
+        hide_meta='null'
+    else
+        hide_meta=$5
+    fi
+
+    if [ -z $6]
+    then
+        tags='null'
+    else
+        tags=$5
+    fi
 fi
 
-if [ -z $3 ]
-then
-    outfile=$lang/blog/articles 
-else
-    outfile=$3
-fi
-
-if [ -e $outfile ]
-then
-    echo `date +%s` $1 | tee -a $outfile
-else
-    echo `date +%s` $1
-fi
+sqlite3 .db/$lang/www.db "insert into $table values ($id, `date +%s`, $path, $pinned, $hide_meta, null, $tags)"
