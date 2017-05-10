88368224f30ce8e81ffad6506bdae582d0aec605
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-10 23:03:03 +0200

    Fixes

--- new-article.sh
+++ new-article.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env sh
-if [ $1 == '-h' ]
-then
-    echo "Usage: new-article <lang> <table> <id> <path> [tags]"
-    echo "Examples:"
-    echo "  new-article en blog hello-world blog/hello-world.html"
-    echo "  new-article en game a-game games/game.html rpg,fun,2d,"
+new_article() {
+    if [ $1 = '-h' ]
+    then
+        echo "Usage: new-article <lang> <table> <id> <path> [tags]"
+        echo "Examples:"
+        echo "  new-article en blog hello-world blog/hello-world.html"
+        echo "  new-article en game a-game games/game.html rpg,fun,2d,"
+    
+        return
+    fi
+    
+    lang=$1
+    table=$2
+    id=$3
+    path=$4
+    pinned='0'
+    hide_meta='0'
+    tags=''
+    
+    sqlite3 .db/$lang/www.db "insert into $table values ('$id', `date +%s`, '$path', $pinned, $hide_meta, null, '$tags')"
+}
 
-    return
-fi
-
-lang=$1
-table=$2
-id=$3
-path=$4
-pinned='0'
-hide_meta='0'
-tags=''
-
-sqlite3 .db/$lang/www.db "insert into $table values ('$id', `date +%s`, '$path', $pinned, $hide_meta, null, '$tags')"
+new_article $1 $2 $3 $3
--- update-article.sh
+++ update-article.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env sh
-if [ $1 == '-h' ]
-then
-    echo "Usage: update-article <lang> <table> <id> [tags]"
-    echo "Examples:"
-    echo "  update-article en blog hello-world"
-    echo "  update-article en game a-game hack-n-slash,"
+update_article() {
+    if [ $1 = '-h' ]
+    then
+        echo "Usage: update-article <lang> <table> <id> [tags]"
+        echo "Examples:"
+        echo "  update-article en blog hello-world"
+        echo "  update-article en game a-game hack-n-slash,"
+    
+        return
+    fi
+    
+    lang=$1
+    table=$2
+    id=$3
+    tags=$4
+    
+    dbtags=`sqlite3 .db/$lang/www.db "select tags from $table where id = '$id'"`
+    tags="${dbtags}${tags}"
+    
+    sqlite3 .db/$lang/www.db "
+        update $table
+        set last_edited = `date +%s`,
+            tags = $tags
+        where id = '$id'"
+}
 
-    return
-fi
-
-lang=$1
-table=$2
-id=$3
-tags=$4
-
-dbtags=`sqlite3 .db/$lang/www.db "select tags from $table where id = '$id'"`
-tags="${dbtags}${tags}"
-
-sqlite3 .db/$lang/www.db "
-    update $table
-    set last_edited = `date +%s`,
-        tags = $tags
-    where id = '$id'"
+update_article $1 $2 $3 $3
