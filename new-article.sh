35c3ff91153a0fe5ee59ee0ccdd8156d9428c2b2
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-10 22:50:27 +0200

    Update scripts

--- new-article.sh
+++ new-article.sh
@@ -1,57 +1,10 @@
 #!/usr/bin/env sh
-if [ -z $1 ]
-then
-    lang=en
-    table=$1
-    id=$2
-    path=$3
+lang=$1
+table=$2
+id=$3
+path=$4
+pinned='null'
+hide_meta='null'
+tags='null'
 
-    if [ -z $4]
-    then
-        pinned='null'
-    else
-        pinned=$4
-    fi
-
-    if [ -z $5]
-    then
-        hide_meta='null'
-    else
-        hide_meta=$4
-    fi
-
-    if [ -z $5]
-    then
-        tags='null'
-    else
-        tags=$4
-    fi
-else
-    lang=$1
-    table=$2
-    id=$3
-    path=$4
-
-    if [ -z $5]
-    then
-        pinned='null'
-    else
-        pinned=$5
-    fi
-
-    if [ -z $6]
-    then
-        hide_meta='null'
-    else
-        hide_meta=$5
-    fi
-
-    if [ -z $6]
-    then
-        tags='null'
-    else
-        tags=$5
-    fi
-fi
-
-sqlite3 .db/$lang/www.db "insert into $table values ($id, `date +%s`, $path, $pinned, $hide_meta, null, $tags)"
+sqlite3 .db/$lang/www.db "insert into $table values ('$id', `date +%s`, '$path', $pinned, $hide_meta, null, '$tags')"
--- update-article.sh
+++ update-article.sh
@@ -1,44 +1,14 @@
 #!/usr/bin/env sh
-if [ -z $1 ]
-then
-    lang=en
-    table=$1
-    id=$2
-    path=$3
-    pinned=$4
-    hide_meta=$4
-    tags=$4
-else
-    lang=$1
-    table=$2
-    id=$3
-    path=$4
-    pinned=$5
-    hide_meta=$5
-    tags=$5
-fi
+lang=$1
+table=$2
+id=$3
+tags=$4
 
-if [ -n $path ]
-then
-    path="path = $path,"
-fi
-
-if [ -n $pinned ]
-then
-    path="pinned = $pinned,"
-fi
-
-if [ -n $hide_meta ]
-then
-    path="hide_meta = $hide_meta,"
-fi
-
-dbtags=`sqlite3 .db/$lang/www.db "select tags from $table where id = $id"`
+dbtags=`sqlite3 .db/$lang/www.db "select tags from $table where id = '$id'"`
 tags="${dbtags}${tags}"
 
 sqlite3 .db/$lang/www.db "
     update $table
     set last_edited = `date +%s`,
-        $path
         tags = $tags
-    where id = $id"
+    where id = '$id'"
