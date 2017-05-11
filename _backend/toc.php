94f6f0a87b8568315282512c863681908d0b3414
pi_pi3 <walter.szymon.98@gmail.com>
2017-05-11 12:32:06 +0200

    Fix and improve toc

--- _backend/toc.php
+++ _backend/toc.php
@@ -7,15 +7,26 @@ echo '<h4>Table of contents</h4>';
 echo '<span style="font-size:80%"><a href="#top">Top</a></span>';
 echo '<hr>';
 
+$has_zero = false;
+foreach ($nav as $h) {
+    if ($h['level'] == 0) {
+        $has_zero = true;
+    }
+}
 
-echo '<ol style="list-style-type:lower-roman;">';
-$level = 0;
+if ($has_zero) {
+    echo '<ol style="list-style-type:lower-roman;">';
+    $level = 0;
+} else {
+    echo '<ol>';
+    $level = 1;
+}
 foreach ($nav as $h) {
     if ($h['level'] > $level) {
-        echo (($level > 1)? '<ul>':'<ol>');
+        echo (($h['level'] > 1)? '<ul>':'<ol>');
         $level = $h['level'];
     } elseif ($h['level'] < $level) {
-        echo (($level > 1)? '</ul>':'</ol>');
+        echo (($h['level'] > 1)? '</ul>':'</ol>');
         $level = $h['level'];
     }
     echo "<a href=\"${h['href']}\">";
